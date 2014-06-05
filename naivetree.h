2efd671eeac45b5c0117c744d3c3f6cc433772b8
Didenko Yakov <kogotto@gmail.com>
2014-06-05 12:22:28 +0400

    some not working code

--- naivetree.h
+++ naivetree.h
@@ -27,14 +27,60 @@ public:
         free(root);
     }
 
+    class BaseFinder {
+    public:
+
+    private:
+        node_pointer_ref plase;
+    };
+
+    void naive_insert(const node_data_t & node_data) {
+        BaseFinder result = find(node_data->key, BaseFinder());
+        result.plase = new node_t(node_data);
+    }
+
+
+    template<class Finder>
+    Finder & find(key_t key, Finder & finder) {
+        node_pointer current = root;
+        while (current != 0) {
+            if (finder(current)) {
+                return finder;
+            }
+            if (key < current->key) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+
+    }
+
+
 private:
     struct node_t:
             public node_data_t {
+
+        node_t(const node_data_t & node_data):
+            node_data_t(node_data),
+            left(0),
+            right(0)
+        {}
+
         node_pointer left;
         node_pointer right;
     };
 
-    static void free(node_)
+    static void free(node_pointer root) {
+        if (root == 0) {
+            return;
+        }
+
+        free(root->left);
+        free(root->right);
+
+        delete root;
+    }
 
     node_pointer root;
 };
